07a4730a84e2499a66f0f8b2e53afd3460766563
John Galt <johngaltfirstrun@gmail.com>
2025-02-25 18:01:25 -0500

    peridot: Suppress spammy log tags
    Change-Id: If903749a07284551a4e97a7927b02a1a
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -340,6 +340,61 @@ PRODUCT_PACKAGES += \
     vendor.qti.qspa-service \
     qspa_vendor.rc
 
+# Spammy log tags - silence on user/userdebug builds
+SPAMMY_LOG_TAGS := \
+    Diag_Lib \
+    artd \
+    ArtService \
+    MiClstc \
+    MiEvent \
+    MiStcImpl \
+    SDM \
+    SRE \
+    libsensor-boledalgo \
+    libsensor-parseRGB \
+    libsensor-qshcalapi \
+    sensors \
+    sensors-hal \
+    vendor.qti.hardware.display.composer-service \
+    vendor.xiaomi.sensor.citsensorservice-service.aidl \
+    vendor.xiaomi.sensor.citsensorservice.aidl \
+    vendor.qti.camera.provider-service_64 \
+    vendor.hardware.vibratorfeature \
+    CamX \
+    CAM_Thumbnail \
+    CAM_CaptureRequestBuilder \
+    CameraLatencyHistogram \
+    ChiX \
+    CHIIQUTILS \
+    CHISEG \
+    CHIUSECASE \
+    DEBUG-portrait_repair \
+    MAW \
+    MiAlgoEngine \
+    mialgo_rfs_api \
+    MiCamHAL \
+    MISV2 \
+    MISV3 \
+    MISV4 \
+    MIGME \
+    MISHisAdapter \
+    android.hardware.power-service.lineage-libperfmgr \
+    libperfmgr \
+    android.hardware.audio.service \
+    android.hardware.light-V2-ndk.vendor \
+    vibratorfeature-wrapper \
+    android.hardware.vibrator-V1-ndk_platform.vendor \
+    libqti-perfd-client \
+    BATTERY_CHG \
+    BackgroundInstallControlService \
+    BackupTransportManager \
+    ActivityManager
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),persist.log.tag.$(tag)=S)
+endif
+
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
