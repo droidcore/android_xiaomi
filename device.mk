854d4b5ddde5073c6cc68d7bfd31e2b0aad2c979
zenin1504 <kayozzprojects@yandex.com>
2025-09-17 12:29:15 +0000

    peridot: Override OTA info in @AxionAOSP
    Change-Id: I5c32492bd77747ff845fb60103ddfd7c
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -302,6 +302,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/com.nxp.mifare.xml
 
+# OTA
+PRODUCT_PACKAGES += PeridotUpdater
+
 # Overlays
 PRODUCT_PACKAGES += \
     ApertureOverlayPeridot \
