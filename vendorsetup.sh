4d08ff3c9b531469597983b2850ee301d084233a
zenin1504 <kayozzprojects@yandex.com>
2025-09-23 15:29:51 +0000

    peridot: Clone kernel source in depth 1
    Change-Id: Ice017c1a7b4d40e08fc8b9181d0a0c57
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ git clone -b lineage-23.0 https://github.com/sm8635-dev/vendor_xiaomi_peridot.gi
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-git clone -b lineage-23.0 https://github.com/sm8635-dev/kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
+git clone -b lineage-23.0 --depth 1 https://github.com/sm8635-dev/kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
 git clone -b lineage-23.0 https://github.com/sm8635-dev/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
