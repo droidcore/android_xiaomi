1668a4d7be8bef4cd28d95d4bd272fc3096c1f6c
droidcore <agusmahardika99@gmail.com>
2025-11-20 18:00:07 +0700

    revert dalvik

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+# Dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # pKVM
 $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
 
@@ -128,15 +131,6 @@ PRODUCT_COPY_FILES += \
 # DSP
 PRODUCT_PACKAGES += \
     DSPVolumeSynchronizer
-
-# Dalvik
-PRODUCT_VENDOR_PROPERTIES += \
-    dalvik.vm.heapstartsize?=16m \
-    dalvik.vm.heapgrowthlimit?=384m \
-    dalvik.vm.heapsize?=512m \
-    dalvik.vm.heaptargetutilization?=0.75 \
-    dalvik.vm.heapminfree?=512k \
-    dalvik.vm.heapmaxfree?=8m
 
 # CameraExtensions
 PRODUCT_COPY_FILES += \
