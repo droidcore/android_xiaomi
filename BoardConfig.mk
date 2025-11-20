3702747b64e73d8e70f92ae343ee159fd8b20c11
droidcore <agusmahardika99@gmail.com>
2025-11-20 16:59:39 +0700

    Revert "peridot: Don't include BoardConfigReservedSize.mk if erofs"
    This reverts commit 8c3748a1b07cd7de892a17a8b663748b8d828eec.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -194,9 +194,7 @@ TARGET_KERNEL_EXT_MODULES := \
 CAMERA_PACKAGE_NAME := com.android.camera
 
 # Partitions
-ifneq ($(PRODUCT_RO_FILE_SYSTEM), erofs)
 -include vendor/lineage/config/BoardConfigReservedSize.mk
-endif
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
